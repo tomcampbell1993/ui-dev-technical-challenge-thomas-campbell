802f13b91b4b1799acff6737368967f3af7df658
tomcampbell1993 <32933040+tomcampbell1993@users.noreply.github.com>
2026-06-08 14:12:03 +0100

    improved styling

--- app/app.vue
+++ app/app.vue
@@ -14,7 +14,14 @@ import {
   VsBody,
 } from '@visitscotland/component-library/components';
 import "/resources/stylesheets/style.css"
-import imageUrl from "/resources/images/edinburgh-castle.jpg"
+
+// Not How I wanted to manage image imports
+import imgEdinCastle from "/resources/images/edinburgh-castle.jpg"
+import imgStirCastle from "/resources/images/stirling-castle.jpg"
+import imgStaffa from "/resources/images/staffa.jpg"
+import imgBrigODoon from "/resources/images/brig-o-doon.jpg"
+import imgStorr from "/resources/images/storr.jpg"
+import imgLochLomond from "/resources/images/loch-lomond.jpg"
 
 const pinia = createPinia()
 const app = createApp(App)
@@ -22,27 +29,39 @@ app.use(pinia)
 
 const cardListMultipleRows = [
   {
-    title: 'Title 1',
-    description: 'Description 1',
-    image: imageUrl,
+    title: 'Edinburgh Castle',
+    description: 'A castle in Edinburgh',
+    image: imgEdinCastle,
     link: '#',
   },
   {
-    title: 'Title 2',
-    description: 'Description 2',
-    image: imageUrl,
+    title: 'Stirling Castle',
+    description: 'A castle in Stirling',
+    image: imgStirCastle,
     link: '#',
   },
   {
-    title: 'Title 3',
-    description: 'Description 3',
-    image: imageUrl,
+    title: 'Staffa',
+    description: 'Very interesting island off coast of scotland',
+    image: imgStaffa,
     link: '#',
   },
   {
-    title: 'Title 4',
-    description: 'Description 4',
-    image: imageUrl,
+    title: 'Brig o\' Doon',
+    description: 'Provides somewhat reliable protection from witches',
+    image: imgBrigODoon,
+    link: '#',
+  },
+  {
+    title: 'The old Man of Storr',
+    description: 'Very nice looking rocks on the Isle of skye',
+    image: imgStorr,
+    link: '#',
+  },
+  {
+    title: 'Loch Lomond',
+    description: 'Traditional ending to scottish weddings',
+    image: imgLochLomond,
     link: '#',
   },
 ]
@@ -60,105 +79,80 @@ function toggleFavorite(card) {
   })
 }
 
-
 </script>
 
 <template>
-  <div>
-    <VsHeading headingStyle="heading-l">UI Developer technical challenge</VsHeading>
-    <VsToggleButton Button icon="fa-regular fa-heart" label="Add to favourites" pressedIcon="fa-solid fa-heart"
-      pressedLabel="Remove from favourites" variant="default" />
+  <div class="wrapper">
+    <div>
+      <VsHeading headingStyle="heading-l">UI Developer technical challenge Thomas Campbell</VsHeading>
+    </div>
+
+    <VsContainer class="mt-075 mt-lg-200 mb-200">
+      <VsRow>
+        <VsCol v-for="(card, index) in cardListMultipleRows" :key="'card-list-multiple-rows-' + index" cols="12" sm="6"
+          lg="4" xl="3" class="mb-400">
+          <VsToggleButton Button icon="fa-regular fa-heart" label="Add to favourites" pressedIcon="fa-solid fa-heart"
+            pressedLabel="Remove from favourites" variant="default" @click="() => toggleFavorite(card)" />
+          <VsCard card-style="outlined">
+            <template #vs-card-header>
+              <img v-if="card.image" :src="card.image"
+                class="w-100 aspect-ratio-3-2 rounded-1 object-fit-cover img-zoom-on-hover" />
+            </template>
+
+            <template #vs-card-body>
+              <div class="px-075">
+                <VsHeading level="3" heading-style="heading-xs">
+                  <VsLink :href="card.link" class="stretched-link" variant="secondary">
+                    {{ card.title }}
+                  </VsLink>
+                </VsHeading>
+
+                <VsBody class="mb-150">
+                  <p class="truncate-2-lines">
+                    {{ card.description }}
+                  </p>
+                </VsBody>
+              </div>
+            </template>
+          </VsCard>
+        </VsCol>
+      </VsRow>
+    </VsContainer>
+
+    <VsHeading headingStyle="heading-m">Favourites</VsHeading>
+
+    <VsContainer class="mt-075 mt-lg-200 mb-200">
+      <VsRow>
+        <VsCol v-for="(card, index) in favoriteList" :key="'card-list-multiple-rows-' + index" cols="12" sm="6" lg="4"
+          xl="3" class="mb-150">
+          <VsCard card-style="outlined">
+            <template #vs-card-header>
+              <img v-if="card.image" :src="card.image"
+                class="w-100 aspect-ratio-3-2 rounded-1 object-fit-cover img-zoom-on-hover" />
+            </template>
+
+            <template #vs-card-body>
+              <div class="px-075">
+                <VsHeading level="3" heading-style="heading-xs">
+                  <VsLink :href="card.link" class="stretched-link" variant="secondary">
+                    {{ card.title }}
+                  </VsLink>
+                </VsHeading>
+
+                <VsBody class="mb-150">
+                  <p class="truncate-2-lines">
+                    {{ card.description }}
+                  </p>
+                </VsBody>
+              </div>
+            </template>
+          </VsCard>
+        </VsCol>
+      </VsRow>
+    </VsContainer>
   </div>
-  <!-- <img src="./resources/images/edinburgh-castle.jpg" /> -->
-
-  <VsCard>
-    <template #vs-card-header>
-      <img src="./resources/images/edinburgh-castle.jpg"
-        class="w-100 aspect-ratio-3-2 rounded-1 object-fit-cover img-zoom-on-hover" />
-    </template>
-
-    <template #vs-card-body>
-      <VsHeading level="3" heading-style="heading-xs">
-        <VsLink href="#" class="stretched-link" variant="secondary" no-visited-styles>
-          Unusual accommodation in Scotland
-        </VsLink>
-      </VsHeading>
-
-      <VsBody>
-        <p class="truncate-2-lines">
-          Fancy staying somewhere a little different? From castles to lighthouses, teepees to brochs, there's lots of
-          unusual choices.
-        </p>
-      </VsBody>
-    </template>
-  </VsCard>
-
-
-
-  <VsContainer class="mt-075 mt-lg-200 mb-200">
-    <VsRow>
-      <VsCol v-for="(card, index) in cardListMultipleRows" :key="'card-list-multiple-rows-' + index" cols="12" sm="6"
-        lg="4" xl="3" class="mb-150">
-        <VsToggleButton Button icon="fa-regular fa-heart" label="Add to favourites" pressedIcon="fa-solid fa-heart"
-          pressedLabel="Remove from favourites" variant="default" @click="() => toggleFavorite(card)" />
-        <VsCard card-style="outlined">
-          <template #vs-card-header>
-            <img v-if="card.image" :src="card.image"
-              class="w-100 aspect-ratio-3-2 rounded-1 object-fit-cover img-zoom-on-hover" />
-          </template>
-
-          <template #vs-card-body>
-            <div class="px-075">
-              <VsHeading level="3" heading-style="heading-xs">
-                <VsLink :href="card.link" class="stretched-link" variant="secondary">
-                  {{ card.title }}
-                </VsLink>
-              </VsHeading>
-
-              <VsBody class="mb-150">
-                <p class="truncate-2-lines">
-                  {{ card.description }}
-                </p>
-              </VsBody>
-            </div>
-          </template>
-        </VsCard>
-      </VsCol>
-    </VsRow>
-  </VsContainer>
-
-  <VsHeading headingStyle="heading-m">Favourites</VsHeading>
-
-  <VsContainer class="mt-075 mt-lg-200 mb-200">
-    <VsRow>
-      <VsCol v-for="(card, index) in favoriteList" :key="'card-list-multiple-rows-' + index" cols="12" sm="6" lg="4"
-        xl="3" class="mb-150">
-        <VsCard card-style="outlined">
-          <template #vs-card-header>
-            <img v-if="card.image" :src="card.image"
-              class="w-100 aspect-ratio-3-2 rounded-1 object-fit-cover img-zoom-on-hover" />
-          </template>
-
-          <template #vs-card-body>
-            <div class="px-075">
-              <VsHeading level="3" heading-style="heading-xs">
-                <VsLink :href="card.link" class="stretched-link" variant="secondary">
-                  {{ card.title }}
-                </VsLink>
-              </VsHeading>
-
-              <VsBody class="mb-150">
-                <p class="truncate-2-lines">
-                  {{ card.description }}
-                </p>
-              </VsBody>
-            </div>
-          </template>
-        </VsCard>
-      </VsCol>
-    </VsRow>
-  </VsContainer>
 </template>
+
 
 <style>
 img {
